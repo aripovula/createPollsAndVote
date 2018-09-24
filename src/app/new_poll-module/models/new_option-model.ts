d44e417579742af26ed89aec369728cac494108d
Ulugbek Aripov <ulugbeks@gmail.com>
2018-09-24 04:13:02 -0400

    before adding additional checklist features

--- src/app/new_poll-module/models/new_option-model.ts
+++ src/app/new_poll-module/models/new_option-model.ts
@@ -3,6 +3,6 @@ export class NewOption {
         public id: number,
         public type: string,
         public text: string,
-        public isSelected: boolean
+        public imageSize: number
     ) {}
 }
